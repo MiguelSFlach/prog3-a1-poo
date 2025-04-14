88d0bcf6ba95e4e7672cb03d3f976d2f2d85de5a
Miguel Schneiders Flach <165222126+MiguelSFlach@users.noreply.github.com>
2025-04-14 19:23:50 -0300

    Add files via upload

---
+++ Autenticador.php
@@ -0,0 +1,20 @@
+<?php
+require_once 'Usuario.php';
+
+class Autenticador {
+    private static $usuarios = [];
+
+    public static function registrar($usuario) {
+        self::$usuarios[] = $usuario;
+    }
+
+    public static function logar($email, $senha) {
+        foreach (self::$usuarios as $usuario) {
+            if ($usuario->autenticar($email, $senha)) {
+                return $usuario;
+            }
+        }
+        return null;
+    }
+}
+?>
